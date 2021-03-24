7a2ef2236d41091a8e3e344ec53973778b65f7fc
Pedro Henrique Vieira Sorrigotti <ypeedroo.vieira@gmail.com>
2021-03-24 20:21:38 -0300

    Alteração do arquivo Exercicio01.java conforme proposto

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,4 +1,4 @@
-import java.awt.event.ActionEvent;
+﻿import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -7,41 +7,44 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
 
-public class Exercicio01 extends JFrame {
+public class Exercicio01 extends JFrame 
+{
 
-public Exercicio01() {
-JMenuBar barraMenu = new JMenuBar();
-JMenuItem opCliente, fechar;
-setTitle("Menu Cadastro");
-setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-setSize( 300,100 );   
-JMenu exers = new JMenu("Cadastrar");
-opCliente = exers.add("Veículos");
-exers.addSeparator();
-fechar = exers.add("Fechar");
-barraMenu.add(exers);
+	public Exercicio01() 
+	{
+		JMenuBar barraMenu = new JMenuBar();
+		JMenuItem opCliente, fechar;
+		setTitle("Menu Cadastro");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setSize( 300,100 );   
+		JMenu exers = new JMenu("Cadastrar");
+		opCliente = exers.add("Veículos");
+		exers.addSeparator();
+		fechar = exers.add("Fechar");
+		barraMenu.add(exers);
+	}
 
-opCliente.addActionListener(new ActionListener() {
-public void actionPerformed(ActionEvent arg0) {
-new Veiculo();
-}
+	opCliente.addActionListener(new ActionListener() 
+	{
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			new Veiculo();
+		}
+	}
 });
-fechar.addActionListener(new ActionListener() {
-public void actionPerformed(ActionEvent arg0) {
-if (JOptionPane .showConfirmDialog(null, "Fechar aplicação?","Fechar", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-System.exit(0);
-}
-}
-});
-setJMenuBar(barraMenu);
-setVisible( true );
-}
 
-public static void main(String[] args){
-Exercicio01 umaJanela = new Exercicio01();
-umaJanela.setBounds(50,100,400,150);
-umaJanela.setResizable(false);
-umaJanela.setLocationRelativeTo(null);
-umaJanela.setVisible(true);
-};
-}
+
+fechar.addActionListener(new ActionListener() 
+{
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		if (JOptionPane .showConfirmDialog(null, "Fechar aplicação?","Fechar", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) 
+		{
+			System.exit(0);
+		}
+	}
+});
+
+
+Editado por Pedro Henrique Vieira Sorrigotti RA: 1902394.
+Data 24/03/2021
